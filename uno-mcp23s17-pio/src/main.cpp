623a4b84771619f0bee453ed6538632a33966c88
Yoshihiro Nakagawa <taku3wzr@gmail.com>
2021-01-06 18:54:28 +0900

    add led on/off
    add led on/off

---
+++ uno-mcp23s17-pio/src/main.cpp
@@ -0,0 +1,16 @@
+#include <Arduino.h>
+
+void setup() {
+    pinMode(LED_BUILTIN, OUTPUT);
+    //Serial.begin(9600);
+    Serial.begin(57600);
+}
+ 
+void loop() {
+    digitalWrite(LED_BUILTIN, HIGH);
+    Serial.println("LED On!!"); 
+    delay(1000); 
+    digitalWrite(LED_BUILTIN, LOW); 
+    Serial.println("LED Off!!");
+    delay(1000); 
+}
